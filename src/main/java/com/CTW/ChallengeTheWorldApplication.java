b136e0484dd101d52ca3e1bb036040552f7922b7
Szabo Ruben <s.r.tailor777@gmail.com>
2019-06-13 14:28:24 +0200

    Project initialization

---
+++ src/main/java/com/CTW/ChallengeTheWorldApplication.java
@@ -0,0 +1,13 @@
+package com.CTW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeTheWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeTheWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CTW/ChallengeTheWorldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.CTW;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChallengeTheWorldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
